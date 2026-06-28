6e6be6f6409f0a5ace234d3ae6f4ed22178c192a
binayakjoshii <binayak.adtu@gmail.com>
2026-06-28 23:39:31 +0530

    cleanup: remove legacy components and redundant pages router login file

--- src/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from "@/components/AuthButton";
-
-export default function LoginPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <AuthForm />
-    </div>
-  );
-}
